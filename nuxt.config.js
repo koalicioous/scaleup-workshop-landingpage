30c5b418e93558addd643c88dd7dec8fbc6ccea6
Surya <surya.negara@gontor.ac.id>
2020-09-18 16:49:08 +0700

    deploy-1

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,9 @@ export default {
   ** See https://nuxtjs.org/api/configuration-target
   */
   target: 'static',
+  router: {
+    base: 'scaleup-workshop-landingpage/'
+  },
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
